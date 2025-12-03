5de3e5b2e9897006ea3e2ad7af55d3a742fc911d
thisiskristina12-cmyk <thisiskristina12@gmail.com>
2025-12-04 00:17:32 +0300

    Реализована функция НОД для RSA

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -34,8 +34,12 @@ def gcd(a: int, b: int) -> int:
     >>> gcd(3, 7)
     1
     """
-    # PUT YOUR CODE HERE
-    pass
+    a, b = abs(a), abs(b)
+    if a == 0 and b == 0:
+        return 0
+    while b != 0:
+        a, b = b, a % b
+    return a
 
 
 def multiplicative_inverse(e: int, phi: int) -> int:
